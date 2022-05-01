7131981aeef6253bfd005918bc6e6fbfcbd0e2d9
makeminds <97055391+makeminds@users.noreply.github.com>
2022-05-01 08:44:50 -0400

    Add files via upload

---
+++ videoscan.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from numpy.core.arrayprint import printoptions
+import pyzbar.pyzbar as pyzbar
+
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+start_point = (5, 5)
+end_point = (635, 475)
+greencolor = (0, 255, 0)
+redcolor = (0, 0, 255)
+thickness = 5
+
+while True:
+    _, frame = cap.read()
+
+
+    decodedObjects = pyzbar.decode(frame)
+    for obj in decodedObjects:
+        code=str(obj.data)
+        location1 = "40.6008623"
+        location2 = "-74.3644696"
+        url = f"b'https://maps.google.com/local?q={location1},{location2}'"
+        if code == url:
+            window_text = "Right Address!"
+            cv2.putText(frame, window_text, (50, 50), font, 4, (0, 255, 0), 3)            
+            cv2.rectangle(frame, start_point, end_point, greencolor, thickness)
+        else:
+            window_text = "Wrong Address!"
+            cv2.putText(frame, window_text, (50, 50), font, 4, (0, 0, 255), 3)
+            cv2.rectangle(frame, start_point, end_point, redcolor, thickness)
+    
+    cv2.imshow("SLAM.AI - MakEMinds", frame)
+        
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+
+
